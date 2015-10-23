9360a90e0632e86c5182574661e6b8cd42bd3ff5
Marcel Sevcovic <marcel@sevcovic.com>
2015-10-23 18:02:28 +0200

    Loader

--- Graph/Graph.DataLoader/Program.cs
+++ Graph/Graph.DataLoader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Graph.DataLoader.Repository;
 
 namespace Graph.DataLoader
 {
@@ -10,6 +11,10 @@ namespace Graph.DataLoader
     {
         static void Main(string[] args)
         {
+            var loader = new LoadRepository();
+            loader.Load();
+            Console.WriteLine("OK");
+            Console.ReadLine();
         }
     }
 }
